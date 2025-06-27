d7a8c8c3cfdf0b8a197e03c2fc0ad924a692a367
Timvnc <tim-vancompernolle@hotmail.com>
2025-06-27 16:38:29 +0200

    update easyconfigs

--- easyblocks/rocmcomponent.py
+++ easyblocks/rocmcomponent.py
@@ -46,6 +46,12 @@ def list_to_cmake_arg(lst):
     """Convert iterable of strings to a value that can be passed as a CLI argument to CMake resulting in a list"""
     return "'%s'" % ';'.join(lst)
 
+HIP_PLATFORM_AMD = "amd"
+HIP_PLATFORM_NVIDIA = "nvidia"
+
+TOOLCHAIN_ROCM_LLVM = "rocm-llvm"
+TOOLCHAIN_HIPCC = "hipcc"
+TOOLCHAIN_DEFAULT = "default"
 
 class EB_ROCmComponent(CMakeMake):
     """Support for building ROCm components"""
@@ -55,8 +61,8 @@ class EB_ROCmComponent(CMakeMake):
         """Extra easyconfig parameters for ROCmComponent"""
         extra_vars = CMakeMake.extra_options(extra_vars)
         extra_vars.update({
-            'use_rocm_llvm_to_build': [True, "Use ROCm-LLVM to build package, uses toolchain compiler otherwise", CUSTOM],
-            'hip_platform': ['amd', "Specify HIP platform. Allowed values: amd, nvidia", CUSTOM],
+            'compiler_toolchain': [TOOLCHAIN_ROCM_LLVM, f"Select toolchain to build the package. Allowed values: {TOOLCHAIN_DEFAULT}, {TOOLCHAIN_ROCM_LLVM}, {TOOLCHAIN_HIPCC}", CUSTOM],
+            'hip_platform': [HIP_PLATFORM_AMD, f"Specify HIP platform. Allowed values: {HIP_PLATFORM_AMD}, {HIP_PLATFORM_NVIDIA}", CUSTOM],
         })
         return extra_vars
 
@@ -64,22 +70,26 @@ class EB_ROCmComponent(CMakeMake):
         """Prepare configuration to properly build ROCm component."""
 
         # If HIP platform is chosen to be nvidia, CUDA should be present in dependencies
-        if self.cfg['hip_platform'] == 'nvidia':
+        if self.cfg['hip_platform'] == HIP_PLATFORM_NVIDIA:
             cuda_root = get_software_root('CUDA')
             if not cuda_root:
                 raise EasyBuildError(f"CUDA is required to build {self.cfg.name} with NVIDIA GPU support!")
-        elif self.cfg['hip_platform'] == 'amd':
+        elif self.cfg['hip_platform'] == HIP_PLATFORM_AMD:
             rocm_llvm_root = get_software_root('ROCm-LLVM')
             if not rocm_llvm_root:
                 raise EasyBuildError(f"ROCm-LLVM is required to build {self.cfg.name} with AMD GPU support!")
         else:
             raise EasyBuildError(f"hip_platform parameter contains non-allowed value.")
 
-        if self.cfg['use_rocm_llvm_to_build']:
+        if self.cfg['compiler_toolchain'] != TOOLCHAIN_DEFAULT:
             if build_option('rpath'):
                 tmp_toolchain = Clang(name='Clang', version='1')
-                tmp_toolchain.COMPILER_CC = 'amdclang'
-                tmp_toolchain.COMPILER_CXX = 'amdclang++'
+                if self.cfg['compiler_toolchain'] == TOOLCHAIN_ROCM_LLVM:
+                    tmp_toolchain.COMPILER_CC = 'clang'
+                    tmp_toolchain.COMPILER_CXX = 'clang++'
+                elif self.cfg['compiler_toolchain'] == TOOLCHAIN_HIPCC:
+                    tmp_toolchain.COMPILER_CC = 'hipcc'
+                    tmp_toolchain.COMPILER_CXX = 'hipcc'
                 tmp_toolchain.prepare_rpath_wrappers()
 
                 cflags = os.getenv('CFLAGS', '')
@@ -87,16 +97,20 @@ class EB_ROCmComponent(CMakeMake):
                 setvar('CFLAGS', "%s %s" % (cflags, '-Wno-unused-command-line-argument'))
                 setvar('CXXFLAGS', "%s %s" % (cxxflags, '-Wno-unused-command-line-argument'))
 
-            amdclang_mock = which('amdclang')
-            amdclangxx_mock = which('amdclang++')
+            if self.cfg['compiler_toolchain'] == TOOLCHAIN_ROCM_LLVM:
+                amdclang_mock = which('clang')
+                amdclangxx_mock = which('clang++')
+            elif self.cfg['compiler_toolchain'] == TOOLCHAIN_HIPCC:
+                amdclang_mock = which('hipcc')
+                amdclangxx_mock = which('hipcc')
 
             self.cfg['configopts'] += f'-DCMAKE_C_COMPILER={amdclang_mock} '
             self.cfg['configopts'] += f'-DCMAKE_CXX_COMPILER={amdclangxx_mock} '
             self.cfg['configopts'] += f'-DCMAKE_HIP_COMPILER={amdclangxx_mock} '
 
         self.cfg['configopts'] += f'-DHIP_PLATFORM={self.cfg["hip_platform"]} '
-        amd_gfx_list =  ['gfx900', 'gfx906', 'gfx908']  
-        if amd_gfx_list and self.cfg['hip_platform'] == 'amd':
+        amd_gfx_list = ['gfx1030']
+        if amd_gfx_list and self.cfg['hip_platform'] == HIP_PLATFORM_AMD:
             # For now, pass both AMDGPU_TARGETS and GPU_TARGETS, until AMD finally drops the former for all packages.
             self.cfg['configopts'] += f'-DAMDGPU_TARGETS={list_to_cmake_arg(amd_gfx_list)} '
             self.cfg['configopts'] += f'-DGPU_TARGETS={list_to_cmake_arg(amd_gfx_list)} '
